1a6dfbf8ac938343d7711951e94feb6536e798f3
CHY <uouo911226@gmail.com>
2022-06-10 15:03:32 +0800

    Hello world with class Person

--- src/main/java/edu/fju/Hello.java
+++ src/main/java/edu/fju/Hello.java
@@ -1,4 +1,9 @@
 package edu.fju;
 
 public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Person p = new Person();
+        p.greeting();
+    }
 }
---
+++ src/main/java/edu/fju/Person.java
@@ -0,0 +1,7 @@
+package edu.fju;
+
+public class Person {
+    public void greeting(){
+        System.out.println("Hello!");
+    }
+}
